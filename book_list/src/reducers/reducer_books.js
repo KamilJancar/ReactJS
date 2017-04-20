085f61b8315f3a1ce1a2cb6d09f511e5544f76b9
kamil jancar <kamiljancar@gmail.com>
2017-04-20 10:30:40 +0200

    book_list

---
+++ book_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: Advance'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Night'},
+        {title: 'Python for begginers'}
+    ]
+}
